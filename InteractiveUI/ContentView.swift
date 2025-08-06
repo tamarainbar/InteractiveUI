a6ebb703ad52ccb11e86d5787a60dc9ffb0be331
Tamara Inbar <tamarainbar@gmail.com>
2025-08-06 15:12:10 -0400

    added an age

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,22 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var name = ""
-    @State private var textTitle = "Enter your name:"
+    @State private var age = ""
+    @State private var textTitle = "Enter your information:"
+    var isSub = false
     var body: some View {
         ZStack {
             //Color(.purple)
-                //.ignoresSafeArea()
+             //   .ignoresSafeArea()
+            
+            Color(.white)
             VStack {
                 Text(textTitle)
                     .font(.title)
                     .foregroundColor(Color.blue)
+                    .multilineTextAlignment(.center)
                 TextField("Type your name here...", text: $name)
                     .multilineTextAlignment(.center)
                     .font(.title)
                     .border(.gray,width: 1)
+                TextField("Type your age here...", text: $age)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .border(.gray,width: 1)
+                
                     .padding()
+                if isSub == true {
+                    Text("submitted")
+                }
                 Button("Submit") {
-                    textTitle = "Welcome \(name)!"
+                    textTitle = "Welcome \(name)!\nYou are \(age)"
+                    //isSub = true
                 }
                 .font(.title2)
                 .buttonStyle(.borderedProminent)
